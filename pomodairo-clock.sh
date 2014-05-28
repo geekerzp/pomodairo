a608940e965d8fb89c57d9f823d4ac40e4e69523
geekerzp <geekerzp@gmail.com>
2014-05-28 22:45:32 +0800

    解决不能重复播放的bug

--- pomodairo-clock.sh
+++ pomodairo-clock.sh
@@ -3,9 +3,13 @@
 #     File Name           :     tomato_clock.sh
 #     Created By          :     geekerzp
 #     Creation Date       :     [2014-05-14 14:06]
-#     Last Modified       :     [2014-05-23 18:10]
+#     Last Modified       :     [2014-05-28 22:44]
 #     Description         :     自制番茄钟
 #######################################################################
+
+#-------------
+# Initialize |
+#-------------
 
 # If a link, fix the path
 TURE_PATH=$(readlink -f $0 | xargs dirname)
@@ -54,31 +58,52 @@ case $SOUND in
         ;;
 esac
 
-# setting clock time
+#---------------------
+# Setting clock time |
+#---------------------
+
 TIME=25
 if [ ! -z $2 ] && [ $2 -gt 0 ] ; then
     TIME=$2
 fi
 
-# starting cmus as background
+#---------------------------------
+# Setting clock background sound |
+#---------------------------------
+
+# starting cmus
 pkill -9 cmus
 ./cmus.sh
 sleep 1s
+
 # clear playlist, library, play queue
 cmus-remote -c && cmus-remote -c -l && cmus-remote -c -q
-# add sound to cmus
-cmus-remote -q $SOUND_PATH
-# play background sound
-cmus-remote -p
-# repeat play
-sleep 1s
-cmus-remote -R
 
-# run
+# add sound to play queue
+cmus-remote -q $SOUND_PATH
+sleep 1s
+
+# play
+cmus-remote -p
+sleep 1s
+
+# repeat
+repeat_status=`cmus-remote -C status | grep repeat | awk '{ print $3 }'`
+if [[ $repeat_status = false ]]; then
+    cmus-remote -R
+fi
+
+#------------
+# Run clock |
+#------------
+
 COMMAND="pkill -9 cmus && env DISPLAY=:0 feh ./assets/pics/tomato.jpg"
 echo $COMMAND | at now+$TIME minutes
 
-# log
+#----------
+# Logging |
+#----------
+
 echo "---------------------------------------------------------"
 echo "               A pomodairo-clock                        "
 echo "                                                        "
